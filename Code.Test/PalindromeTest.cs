2d94cb4a4c8e4befbffb2fb6b895577098090212
Amit Chauhan <achauhan@microsoft.com>
2019-04-22 15:21:14 +0530

    adding palindrome

---
+++ Code.Test/PalindromeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Code.Test
+{
+    [TestClass]
+    public class PalindromeTest
+    {
+        [TestMethod]
+        public void SampleTest()
+        {
+            var s = File.ReadAllText(@"f:\Pactice\test.txt");
+            var result = Palindrome.palindromeIndex("hgygsvlfcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcwflvsgygh");
+            Assert.AreEqual(44, result);
+        }
+    }
+}
---
+++ Code/Palindrome.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public static class Palindrome
+    {
+        public static int palindromeIndex(string s)
+        {
+            if (isPalinDrome(s))
+                return -1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[0 + i] != s[s.Length - 1 - i])
+                {
+                    if (isPalinDrome(s.Substring(i + 1, (s.Length - 1 - i)-(i ))))
+                        return i;
+                    if (isPalinDrome(s.Substring(i , (s.Length - 1 - i) - (i))))
+                        return s.Length - 1 - i ;
+                }
+            }
+            return -1;
+        }
+        private static bool isPalinDrome(string s)
+        {
+            if(s.Length <= 1)
+                return true;
+            for(int i = 0; i < s.Length/2; i++)
+            {
+                if (s[0 + i] != s[s.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
