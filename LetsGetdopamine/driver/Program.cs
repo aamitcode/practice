7f2e2aef790949c3d4f66b90096140fe4ed9665c
unknown <chauhan.amit@outlook.com>
2023-12-16 15:21:44 +0000

    adding driver

---
+++ LetsGetdopamine/driver/Program.cs
@@ -0,0 +1,6 @@
+﻿using BasicCalculator;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var target = new BasicCalculator.BasicCalculator();
+var result = target.TestOne("2+2+2+2-9");
+Console.WriteLine(result);
