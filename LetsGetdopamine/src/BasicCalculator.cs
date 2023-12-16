8e4a78760bd5f8ea00d964eeeb9468bd931a7bbf
unknown <chauhan.amit@outlook.com>
2023-12-16 15:11:02 +0000

    adding basic calc draft

---
+++ LetsGetdopamine/src/BasicCalculator.cs
@@ -0,0 +1,54 @@
+﻿namespace BasicCalculator;
+
+public class BasicCalculator
+{
+    public int TestOne(string mathExpression){
+        int leftNumber = 0;
+        int rightNumber = 0;
+        bool operationFound =false;
+        char operation ='+';
+        int result =0;
+        foreach(var item in mathExpression){
+            if(isDigit(item)){
+                if(!operationFound){
+                    leftNumber = leftNumber*10 + item -'0';
+                }
+                if(operationFound){
+                    rightNumber = rightNumber*10 + item-'0';
+                }
+            }
+            else{
+                if(!operationFound){
+                    operationFound=true;
+                }
+                else{
+                    leftNumber = performOperation(leftNumber,rightNumber,operation);
+                    rightNumber=0;
+                }
+                operation = item;
+            }
+        }
+       return performOperation(leftNumber,rightNumber,operation);
+    }
+
+    public static bool isDigit(char c){
+        if('0'<=c && '9'>=c ){
+            return true;
+        }
+        return false;
+    }
+
+    public static int performOperation(int leftNumber, int rightNumber, char operation){
+         int result=0;
+        switch(operation){
+            case '+':
+            result = leftNumber + rightNumber;
+            break;
+            case '-':
+            result =leftNumber - rightNumber;
+            break;
+        }
+        return result;
+    }
+
+}
---
+++ LetsGetdopamine/test/BasicCalculatorTest.cs
@@ -0,0 +1,18 @@
+namespace test;
+using BasicCalculator;
+
+[TestClass]
+public class BasicCalculatorTest
+{
+    [TestMethod]
+    public void BaseTest()
+    {
+        var target = new BasicCalculator();
+        int firstNumber = 18;
+        int secondNumber =2;
+        int thirdNumber=4;
+        int forthNumber =5;
+        var result = target.TestOne($"{firstNumber}+{secondNumber}+{thirdNumber}-{forthNumber}");
+        Assert.AreEqual(firstNumber+secondNumber+thirdNumber-forthNumber,result);
+    }
+}
