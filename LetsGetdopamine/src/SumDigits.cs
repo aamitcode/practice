7d49df8d6e7de01beba574b607d293007e51d873
Amit Chauhan <chauhan.amit@outlook.com>
2023-12-20 16:05:38 +0000

    sum digit

---
+++ LetsGetdopamine/src/SumDigits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class SumDigits
+    {
+        public static int Sum(int num)
+        {
+            if (num < 10)
+            {
+                return num;
+            }
+            else
+            {
+                int sum = 0;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+                return Sum(sum);
+            }
+        }
+    }
+}
---
+++ LetsGetdopamine/test/SumDigitTest.cs
@@ -0,0 +1,49 @@
+﻿using src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    [TestClass]
+    public class SumDigitTest
+    {
+        [TestMethod]
+        public void TestSum1()
+        {
+            Assert.AreEqual(4, SumDigits.Sum(49));
+        }
+
+        [TestMethod]
+        public void TestSum2()
+        {
+            Assert.AreEqual(1, SumDigits.Sum(1));
+        }
+
+        [TestMethod]
+        public void TestSum3()
+        {
+            Assert.AreEqual(3, SumDigits.Sum(439230));
+        }
+
+        [TestMethod]
+        public void TestSum4()
+        {
+            Assert.AreEqual(1, SumDigits.Sum(1000000000));
+        }
+
+        [TestMethod]
+        public void TestSum5()
+        {
+            Assert.AreEqual(0, SumDigits.Sum(0));
+        }
+
+        [TestMethod]
+        public void TestSum6()
+        {
+            Assert.AreEqual(1, SumDigits.Sum(int.MaxValue));
+        }
+    }
+}
