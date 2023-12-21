2ca2328bfea8ac726bc9f7422861083cfcb0f4a0
Amit Chauhan <chauhan.amit@outlook.com>
2023-12-21 13:40:48 +0000

    three sum solution

---
+++ LetsGetdopamine/src/Solution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+
+            //[-1,0,1,2,-1,-4]
+            //[-1,0,0,1]
+            IList<IList<int>> ret = new List<IList<int>>();
+            Array.Sort(nums);
+            for(int i=0;i< nums.Length - 2; i++)
+            {
+                if(i>0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                int target = 0 - nums[i];
+                int j = i + 1;
+                int k = nums.Length - 1;
+                while (j < k)
+                {
+                    if(j>i+1 && nums[j] == nums[j - 1])
+                    {
+                        j++;
+                        continue;
+                    }
+                    if(k< nums.Length-1 && nums[k] == nums[k + 1])
+                    {
+                        k--;
+                        continue;
+                    }
+                    if (nums[j] + nums[k] == target)
+                    {
+                        List<int> temp = new List<int>();
+                        temp.Add(nums[i]);
+                        temp.Add(nums[j]);
+                        temp.Add(nums[k]);
+                        j++;
+                        ret.Add(temp);
+                    }
+                    else
+                    {
+                        if(target < nums[j] + nums[k])
+                        {
+                            k--;
+                        }
+                        else
+                        {
+                            j++;
+                        }
+                    }
+
+                }
+            }
+            return ret;
+        }
+    }
+}
---
+++ LetsGetdopamine/test/SolutionTest.cs
@@ -0,0 +1,21 @@
+﻿using src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void Test3Sum()
+        {
+            var target = new Solution();
+            var result = target.ThreeSum([-1, 0, 1, 0]);
+            Assert.AreEqual(1, result.Count);
+        }
+    }
+}
